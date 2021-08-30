926e06a0b664de3c6941a30d8989bb139dbba9c3
matplo <ploskon@gmail.com>
2021-08-30 01:55:57 -0700

    functions into a namespace - TGlauberMC_f - clarify for python... ROOT.TGlauberMC_f.runAndSaveNtuple() for example

--- pyjetty_TGlauberMC_LinkDef.h
+++ pyjetty_TGlauberMC_LinkDef.h
@@ -4,6 +4,13 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ namespace TGlauberMC_f;
+#pragma link C++ function TGlauberMC_f::runAndSaveNtuple;
+#pragma link C++ function TGlauberMC_f::runAndSaveNucleons;
+#pragma link C++ function TGlauberMC_f::runAndSmearNtuple;
+#pragma link C++ function TGlauberMC_f::runAndOutputLemonTree;
+#pragma link C++ function TGlauberMC_f::runAndCalcDens;
+
 #pragma link C++ class TGlauNucleon+;
 #pragma link C++ class TGlauNucleus+;
 #pragma link C++ class TGlauberMC+;
--- tglaubermc.hh
+++ tglaubermc.hh
@@ -107,68 +107,6 @@ using namespace std;
 #endif
 
 //---------------------------------------------------------------------------------
-TF1 *getNNProf(Double_t snn=67.6, Double_t omega=0.4, Double_t G=1);
-
-//---------------------------------------------------------------------------------
-void runAndSaveNtuple(const Int_t n,
-                      const char *sysA        = "Pb",
-                      const char *sysB        = "Pb",
-                      const Double_t signn    = 67.6,
-                      const Double_t sigwidth = -1,
-                      const Double_t mind     = 0.4,
-		      const Double_t omega    = -1,
-                      const Double_t noded    = -1,
-                      const char *fname       = 0);
-
-//---------------------------------------------------------------------------------
-void runAndSaveNucleons(const Int_t n,                    
-                        const char *sysA        = "Pb",           
-                        const char *sysB        = "Pb",           
-                        const Double_t signn    = 67.6,           
-                        const Double_t sigwidth = -1,
-                        const Double_t mind     = 0.4,
-                        const Bool_t verbose    = 0,
-			const Double_t bmin     = 0.0,
-			const Double_t bmax     = 20.0,
-                        const char *fname       = 0);
-
-//---------------------------------------------------------------------------------
-void runAndSmearNtuple(const Int_t n,
-                       const Double_t sigs  = 0.4,
-                       const char *sysA     = "p",
-                       const char *sysB     = "Pb",
-                       const Double_t signn = 67.6,
-                       const Double_t mind  = 0.4,
-		       const Double_t bmin  = 0.0,
-		       const Double_t bmax  = 20.0,
-                       const char *fname    = 0);
-
-
-//---------------------------------------------------------------------------------
-void runAndOutputLemonTree(const Int_t n,
-			   const Double_t sigs  = 0.4,
-			   const char *sysA     = "p",
-			   const char *sysB     = "Pb",
-			   const Double_t signn = 67.6,
-			   const Double_t mind  = 0.4,
-			   const Double_t bmin  = 0.0,
-			   const Double_t bmax  = 20.0,
-			   const Bool_t   ogrid = 0,
-			   const char *fname    = 0);
-
-//---------------------------------------------------------------------------------
-void runAndCalcDens(const Int_t n,
-		    const Double_t alpha = 0.1,
-		    const char *sysA     = "Pb",
-		    const char *sysB     = "Pb",
-		    const Double_t signn = 67.6,
-		    const Double_t mind  = 0.4,
-		    const char *fname    = "glau_dens_hists.root");
-
-
-//---------------------------------------------------------------------------------
-
-//---------------------------------------------------------------------------------
 class TGlauNucleon : public TObject
 {
   protected:
@@ -453,3 +391,5 @@ class TGlauberMC : public TNamed
 
     ClassDef(TGlauberMC,6) // TGlauberMC class
 };
+
+#endif
